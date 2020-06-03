9af5f7d242d27bcbe8b81a388485a6b0cd7c9cef
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-06-03 22:27:51 +0900

    fetch projects

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # redmine
+
+## Installation
+
+clone project
+
+```zsh
+git clone https://github.com/lsnl/redmine.git
+cd redmine
+```
+
+install requre modules
+
+```zsh
+pip install -r requirements.txt
+```
+
+set REDMINE_API_ACCESS_KEY to your environment variable
+
+> You can see your API access key in http://rm.lsnl.jp/my/account
+
+```zsh
+export REDMINE_API_ACCESS_KEY="XXX"
+```
+
+## Usage
+
+```zsh
+
+```
---
+++ redmine.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+
+from redminelib import Redmine
+
+SERVER_URL = 'http://rm.lsnl.jp/'
+API_ACCESS_KEY = os.getenv('REDMINE_API_ACCESS_KEY')
+
+
+def fetch_projects(redmine, query=None):
+    # TODO: separate processing by query
+
+    return redmine.project.all()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('commands', nargs='+')
+    args = parser.parse_args()
+    commands = vars(args)['commands']
+
+    if len(commands) < 1:
+        parser.print_help()
+
+    if API_ACCESS_KEY is None:
+        print('please set REDMINE_API_ACCESS_KEY')
+        sys.exit(1)
+
+    redmine = Redmine(SERVER_URL, key=API_ACCESS_KEY)
+
+    if commands[0] == 'projects':
+        projects = fetch_projects(redmine, commands[1])
+        print('\n'.join(map(str, projects)))
+
+
+if __name__ == '__main__':
+    main()
